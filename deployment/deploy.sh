8f65c8d8b6bcfa6dea9b8533417f0ca1136c82d8
mantre <mantre@protonmail.com>
2024-12-27 15:20:33 +0800

    chore: remove unnecessary commented code

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -15,10 +15,6 @@ echo "Building ${DOCKER_TAG} version"
 docker build -t pagu-discord:${DOCKER_TAG}  -f ./deployment/Dockerfile . --target discord
 docker build -t pagu-telegram:${DOCKER_TAG} -f ./deployment/Dockerfile . --target telegram
 
-# docker buildx build \
-#   --target discord -t pagu-discord \
-#   --target telegram -t pagu-telegram -f ./deployment/Dockerfile .
-
 docker compose -f ./deployment/docker-compose.yml down
 docker compose -f ./deployment/docker-compose.yml up -d
 
