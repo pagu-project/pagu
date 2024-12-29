e962d00c76d26e41658af24fd2f09e9b808a428e
sepehr-dh99 <sepehr.dh99@gmail.com>
2024-12-29 22:16:21 +0330

    chore: remove meta from versioning

--- version.go
+++ version.go
@@ -3,7 +3,6 @@ package pagu
 import "fmt"
 
 type Version struct {
-	Meta  string
 	Major uint8
 	Minor uint8
 	Patch uint8
@@ -13,15 +12,10 @@ var version = Version{
 	Major: 0,
 	Minor: 0,
 	Patch: 1,
-	Meta:  "beta",
 }
 
 func StringVersion() string {
 	ver := fmt.Sprintf("%d.%d.%d", version.Major, version.Minor, version.Patch)
-
-	if version.Meta != "" {
-		ver = fmt.Sprintf("%s-%s", ver, version.Meta)
-	}
 
 	return ver
 }
