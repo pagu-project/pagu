7931d4118d527d0aa85c9f9a7093148dd661debd
Mohammad Hasan Saeedkia <170322053+MHSaeedkia@users.noreply.github.com>
2025-01-18 21:55:48 +0330

    FIX : lint issue

--- internal/engine/command/market/market.go
+++ internal/engine/command/market/market.go
@@ -21,14 +21,13 @@ func NewMarketCmd(clientMgr client.IManager, priceCache cache.Cache[string, enti
 	}
 }
 
-func (c *MarketCmd) GetCommand() *command.Command {
-
-	cmd := c.buildMarketCommand()
+func (m *MarketCmd) GetCommand() *command.Command {
+	cmd := m.buildMarketCommand()
 	cmd.AppIDs = entity.AllAppIDs()
 	cmd.TargetFlag = command.TargetMaskMainnet
 
-	c.subCmdPrice.AppIDs = entity.AllAppIDs()
-	c.subCmdPrice.TargetFlag = command.TargetMaskMainnet
+	m.subCmdPrice.AppIDs = entity.AllAppIDs()
+	m.subCmdPrice.TargetFlag = command.TargetMaskMainnet
 
 	return cmd
 }
