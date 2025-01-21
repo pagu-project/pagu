017118a913ad46888d61852085de32ff66536f2c
Sepehr Dehghani <55226923+sepehr-dh99@users.noreply.github.com>
2025-01-21 13:30:19 +0330

    feat: new cli platform (#285)

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,22 +1,16 @@
 package main
 
 import (
-	"bufio"
-	"os"
-	"strings"
-
 	pagucmd "github.com/pagu-project/pagu/cmd"
 	"github.com/pagu-project/pagu/config"
 	"github.com/pagu-project/pagu/internal/engine"
-	"github.com/pagu-project/pagu/internal/entity"
+	"github.com/pagu-project/pagu/internal/platforms/cli"
 	"github.com/pagu-project/pagu/internal/version"
 	"github.com/pagu-project/pagu/pkg/log"
 	"github.com/spf13/cobra"
 )
 
 var configPath string
-
-const PROMPT = "\n>> "
 
 func run(cmd *cobra.Command, _ []string) {
 	configs, err := config.Load(configPath)
@@ -29,24 +23,7 @@ func run(cmd *cobra.Command, _ []string) {
 
 	botEngine.Start()
 
-	reader := bufio.NewReader(os.Stdin)
-
-	for {
-		cmd.Print(PROMPT)
-
-		input, _ := reader.ReadString('\n')
-		input = strings.TrimSuffix(input, "\n")
-
-		if strings.EqualFold(input, "exit") {
-			cmd.Println("exiting from cli")
-
-			return
-		}
-
-		response := botEngine.ParseAndExecute(entity.PlatformIDCLI, "0", input)
-
-		cmd.Printf("%v\n%v", response.Title, response.Message)
-	}
+	cli.HandleCliCommands(cmd, botEngine)
 }
 
 func main() {
