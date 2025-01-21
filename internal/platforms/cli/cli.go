9b4c9211b3872833ec3bb73bd7fa0738e4884d43
Sepehr <sepehr.dh99@gmail.com>
2025-01-21 12:12:23 +0330

    feat: new cli platform

---
+++ internal/platforms/cli/cli.go
@@ -0,0 +1,35 @@
+package cli
+
+import (
+	"bufio"
+	"os"
+	"strings"
+
+	"github.com/pagu-project/pagu/internal/engine"
+	"github.com/pagu-project/pagu/internal/entity"
+	"github.com/spf13/cobra"
+)
+
+const PROMPT = "\n>> "
+
+func HandleCliCommands(cmd *cobra.Command, botEngine *engine.BotEngine) {
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		cmd.Print(PROMPT)
+
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSuffix(input, "\n")
+
+		if strings.EqualFold(input, "exit") {
+			cmd.Println("exiting from cli")
+
+			return
+		}
+
+		response := botEngine.ParseAndExecute(entity.PlatformIDCLI, "0", input)
+
+		cmd.Printf("%v\n%v", response.Title, response.Message)
+	}
+}
