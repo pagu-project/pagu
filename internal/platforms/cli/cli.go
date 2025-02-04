4219604d61a4735c0f89e6b186ffd06a56427333
b00f <mostafa.sedaghat@gmail.com>
2025-02-04 13:12:47 +0800

    Revert "style: format result on cli using glow (#309)"
    This reverts commit 704a4e457e0f49ba1571784dc9e716b983ed5fbe.

--- internal/platforms/cli/cli.go
+++ internal/platforms/cli/cli.go
@@ -2,10 +2,7 @@ package cli
 
 import (
 	"bufio"
-	"bytes"
-	"fmt"
 	"os"
-	"os/exec"
 	"strings"
 
 	"github.com/pagu-project/pagu/internal/engine"
@@ -19,7 +16,6 @@ func HandleCliCommands(cmd *cobra.Command, botEngine *engine.BotEngine) {
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
-		chatHistory := bytes.Buffer{}
 		cmd.Print(PROMPT)
 
 		input, _ := reader.ReadString('\n')
@@ -33,18 +29,6 @@ func HandleCliCommands(cmd *cobra.Command, botEngine *engine.BotEngine) {
 
 		response := botEngine.ParseAndExecute(entity.PlatformIDCLI, "0", input)
 
-		chatHistory.WriteString(fmt.Sprintf("%v\n%v", response.Title, response.Message))
-
-		// Pass response to Glow via stdin
-		command := exec.Command("glow")
-		if command.Err != nil {
-			cmd.Printf("%v\n%v", response.Title, response.Message)
-
-			continue
-		}
-		command.Stdin = strings.NewReader(chatHistory.String())
-		command.Stdout = os.Stdout
-		command.Stderr = os.Stderr
-		_ = command.Run()
+		cmd.Printf("%v\n%v", response.Title, response.Message)
 	}
 }
