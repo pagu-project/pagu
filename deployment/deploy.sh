565eb4b2545d029c437cf9dd6c9cb029352c8732
Mantre <mantre@protonmail.com>
2025-05-31 06:21:43 +0800

    ci: restart docker2host (#385)

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -5,11 +5,9 @@ set -e
 # Ensure Database is up and running
 docker compose -f ./deployment/docker-compose.db.yml up -d
 
-
+# Is it stable or latest?
 export DEPLOY_TAG="latest"
-
 TAG=$(git describe --tags --exact-match 2> /dev/null) || echo ""
-
 if [[ -n "$TAG" ]]; then
     export DEPLOY_TAG="stable"
 fi
@@ -18,6 +16,9 @@ echo "Building ${DEPLOY_TAG} version..."
 docker build -t pagu:${DEPLOY_TAG} -f ./deployment/Dockerfile .
 
 docker compose -p pagu_${DEPLOY_TAG} -f ./deployment/docker-compose.yml up -d
+
+# restart docker2host service
+systemctl --user restart docker2host.service
 
 ## Some cleanup
 echo "Cleanup..."
